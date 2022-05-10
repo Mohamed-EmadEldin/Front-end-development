a3f163af002693955b08c2133fb3b49696e303a0
Mohamed-EmadEldin <mohamed.emad1719@gmail.com>
2022-05-10 23:09:13 +0200

    creat student component

--- Angular/Demo1/src/app/app.module.ts
+++ Angular/Demo1/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StudentComponent } from './Student/Student.component';
+import { DepartementComponent } from './departement/departement.component';
 
 @NgModule({
   declarations: [
-    AppComponent,StudentComponent
+    AppComponent,StudentComponent, DepartementComponent
   ],
   imports: [
     BrowserModule
