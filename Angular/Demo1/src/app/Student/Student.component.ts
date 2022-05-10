f3e0d57964843705272cd6be28a3b93211b94064
Mohamed-EmadEldin <mohamed.emad1719@gmail.com>
2022-05-10 22:52:37 +0200

    creat class studentcompont and import it and use selector

---
+++ Angular/Demo1/src/app/Student/Student.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "xyz",
+  template :`
+  <h1>Student Component Works dude</h1>
+
+  `,
+})
+export class StudentComponent{
+
+}
--- Angular/Demo1/src/app/app.module.ts
+++ Angular/Demo1/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StudentComponent } from './Student/Student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,StudentComponent
   ],
   imports: [
     BrowserModule
