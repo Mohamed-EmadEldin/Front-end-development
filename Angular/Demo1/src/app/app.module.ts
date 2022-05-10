ec8db9fb477cdc8d6d6efa6beca295b3a93cc36e
Mohamed-EmadEldin <mohamed.emad1719@gmail.com>
2022-05-11 00:17:35 +0200

    make components of RedTie

--- Angular/Demo1/src/app/app.module.ts
+++ Angular/Demo1/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { StudentComponent } from './Student/Student.component';
 import { DepartementComponent } from './departement/departement.component';
+import { HeaderComponent } from './header/header.component';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+import { MaintextComponent } from './maintext/maintext.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,StudentComponent, DepartementComponent
+    AppComponent,StudentComponent, DepartementComponent, HeaderComponent, SidemenuComponent, MaintextComponent, FooterComponent
   ],
   imports: [
     BrowserModule
